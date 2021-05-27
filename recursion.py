33175712b03af9edf51008522745f2cf60d21907
Esther Wavinya <estherwavinya80@gmail.com>
2021-05-27 10:16:45 +0300

    recursion loops

--- recursion.py
+++ recursion.py
@@ -95,3 +95,63 @@ Here is your output:
 
 Great work! You've really nailed writing recursive
 functions!
+
+
+
+Question
+The function sum_positive_numbers should return the sum of all positive numbers between the number n received and 1. For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15. Fill in the gaps to make this work:
+
+
+def sum_positive_numbers(n):
+    # The base case is n being smaller than 1
+    if n < 1:
+        return n
+
+    # The recursive case is adding this number to 
+    # the sum of the numbers smaller than this one.
+    return n + sum_positive_numbers(n - 1)
+
+print(sum_positive_numbers(3)) # Should be 6
+
+
+Here is your output:
+6
+15
+
+Whoohoo! You've just written your first recursive function.
+Well done!
+
+
+#Question
+Which of the following scenarios would benefit the most from using a recursive function to solve the problem?
+
+You need to create a family tree, showing several generations of your ancestors, with all of their children.
+
+Correct
+Great job! You're getting the concept of recursion and when it's a better solution than the traditional looping techniques.
+
+
+
+#Additional Recursion Sources
+#Additional Recursion Sources
+In the past videos, we visited the basic concepts of recursive functions.
+
+A recursive function must include a recursive case and base case. The recursive case calls the function again, with a different value. The base case returns a value without calling the same function.
+
+A recursive function will usually have this structure:
+
+
+def recursive_function(parameters):
+    if base_case_condition(parameters):
+        return base_case_value
+    recursive_function(modified_parameters)
+
+
+For more information on recursion, check out these resources:
+
+Wikipedia Recursion page
+See what happens when you Search Google for Recursion
+
+
+
+
