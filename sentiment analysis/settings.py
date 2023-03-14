c9f689fdf390f03219cb7bf35cbda375e0645aa6
Esther-Wavinya <estherwavinya80@gmail..com>
2023-03-14 14:38:00 +0300

    jupyter

---
+++ sentiment analysis/settings.py
@@ -0,0 +1,6 @@
+TRACK_WORDS = ['Facebook']
+TABLE_NAME = "Facebook"
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
+            polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
+            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
+            retweet_count INT, favorite_count INT"
